92163a5edde59177bc08e1b93ae3c60fc4dad609
Alisa Schink <Alisa Schink>
2017-02-26 16:01:21 -0500

    placeholders for survey questions included

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -40,10 +40,10 @@ module.exports = function(app) {
   // I added this below code so you could clear out the table while working with the functionality.
   // Don"t worry about it!
 
-  app.post("/api/clear", function() {
-    // Empty out the arrays of data
-    friendsData = [];
+//   app.post("/api/clear", function() {
+//     // Empty out the arrays of data
+//     friendsData = [];
     
-    console.log(friendsData);
-  });
+//     console.log(friendsData);
+//   });
 };
