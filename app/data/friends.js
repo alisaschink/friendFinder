9deb64e6ffc299d06b1ac8b3c6748475b1881edb
Alisa Schink <Alisa Schink>
2017-02-27 16:30:32 -0500

    friends added

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,21 +2,53 @@
 
 var friendsData = [
   {
-    name: "test",
-    photo: "#",
+    name: "Freddy",
+    photo: "https://s-media-cache-ak0.pinimg.com/736x/a5/22/5c/a5225c0b1213f21a245ea918e089620d.jpg",
     scores: [
+      "5",
       "1",
+      "4",
       "2",
       "2",
+      "3",
+      "2",
       "1",
-      "1",
-      "1",
-      "1",
-      "1",
-      "1",
-      "1"
+      "2",
+      "4"
     ]
   },
+  {
+    name: "Jason",
+    photo: "https://upload.wikimedia.org/wikipedia/en/thumb/0/05/Jasonf.jpg/250px-Jasonf.jpg",
+    scores: [
+      "2",
+      "4",
+      "4",
+      "4",
+      "4",
+      "4",
+      "4",
+      "4",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Michael",
+    photo: "http://www.lovethispic.com/uploaded_images/38159-Michael-Myers-.jpg",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "2",
+      "4",
+      "2",
+      "1",
+      "3",
+      "3"
+    ]
+  }
 ];
 
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,7 +13,8 @@ module.exports = function(app) {
   app.post("/api/friends", function(req, res) {
       friendsData.push(req.body);
       // res.json(true);
-      // compares scores between new user and friends data 
+      // compares scores between new user and friends data
+      var diffScores = []; 
       var lastIndex = friendsData.length - 1;
       var arr1 = friendsData[lastIndex].scores
       for(i=0; i<friendsData.length-1; i++){
@@ -23,7 +24,19 @@ module.exports = function(app) {
           var quesScore = Math.abs(parseInt(arr1[j]) - parseInt(arr2[j]));
           totalScore += quesScore;
         } 
-        console.log(totalScore)
+        diffScores.push(totalScore)
       }
+
+      // selects user with closest matching scores
+      var lowestScore = 40;
+      for(i=0; i<diffScores.length; i++){
+        if(diffScores[i] < lowestScore){
+          lowestScore = diffScores[i];
+        }
+      }
+      var lowestScoreIndex = diffScores.indexOf(lowestScore);
+      var bestMatchName = friendsData[lowestScoreIndex].name;
+      var bestMatchPhoto = friendsData[lowestScoreIndex].photo;
+      console.log(bestMatchName);
   });
 };
