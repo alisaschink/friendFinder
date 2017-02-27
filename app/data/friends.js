4ee82a9efd45cd3d8f0f0f8c25853e00578d2585
Alisa Schink <Alisa Schink>
2017-02-27 15:35:46 -0500

    score comparison works

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,25 @@
 // holds friends survey data
 
 var friendsData = [
-
+  {
+    name: "test",
+    photo: "#",
+    scores: [
+      "1",
+      "2",
+      "2",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1"
+    ]
+  },
 ];
+
+
 
 // exports array of friends objects.
 module.exports = friendsData;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,6 +12,18 @@ module.exports = function(app) {
   // post Request for when user submits survey
   app.post("/api/friends", function(req, res) {
       friendsData.push(req.body);
-      res.json(true);
+      // res.json(true);
+      // compares scores between new user and friends data 
+      var lastIndex = friendsData.length - 1;
+      var arr1 = friendsData[lastIndex].scores
+      for(i=0; i<friendsData.length-1; i++){
+        var arr2 = friendsData[i].scores
+        var totalScore = 0;
+        for(j=0; j<arr1.length; j++){
+          var quesScore = Math.abs(parseInt(arr1[j]) - parseInt(arr2[j]));
+          totalScore += quesScore;
+        } 
+        console.log(totalScore)
+      }
   });
 };
